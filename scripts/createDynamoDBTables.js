25d4bcdea31a689362bc3eecb3f5fcdd47092857
Liron Avraham <lironamy@gmail.com>
2025-11-26 15:32:32 +0200

    123

--- scripts/createDynamoDBTables.js
+++ scripts/createDynamoDBTables.js
@@ -1,12 +1,10 @@
 const { DynamoDBClient, CreateTableCommand, DescribeTableCommand } = require('@aws-sdk/client-dynamodb');
+const { fromNodeProviderChain } = require('@aws-sdk/credential-providers');
 require('dotenv').config();
 
 const client = new DynamoDBClient({
     region: process.env.AWS_REGION || 'us-east-1',
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    credentials: fromNodeProviderChain()
 });
 
 const tables = [
